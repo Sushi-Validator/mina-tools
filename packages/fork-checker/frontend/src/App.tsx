65cf4a618215b4a71a42f46f973ff4af22789223
Norkkom <CATBIRDS@users.noreply.github.com>
2021-05-10 13:34:45 -0400

    Further work on App.tsx
    - Implemented page routing using react-router
    - Updated readout to be a bit cleaner, albeit still not pretty due to lack of any CSS
    - Implemented clickthrough for forks that will redirect to the fork page appropriate for a specific fork
    - Made max shown forks a function parameter so it's easier to modify on-the-fly for user customization; this might also be useful to implement for the date filter (i.e. allowing users to view forks from the past week/day/month based on user selection)
    - Implemented API call on fork page for rudimentary (and currently incomplete) data collection on a given fork
    - Created skeleton for Producer page

--- packages/fork-checker/frontend/src/App.tsx
+++ packages/fork-checker/frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import $ from 'jquery';
-
+import { Redirect, Link, Route, BrowserRouter as Router } from 'react-router-dom';
 
 // Big ugly monster, clean this up later
-// TODO: Cleanup, prettify output, add clickthrough per entry
-function Update() {
-  let last_week = Date.now() - 604800;
+// TODO: Cleanup, sort output by timestamp, determine method for grabbing latest producer and displaying it
+function Update(max: any) {
+  // 'max' parameter dictates how many forks we want to display at a time
+  let last_week = Math.floor((Date.now() / 1000) - 604800);
   let API = 'https://fork-checker-api-iyrf2hryca-uw.a.run.app/forks?';
   let DEFAULT_QUERY = 'updated_after=' + last_week + '&min_length=5'; // Get forks from within the past week that are length >= 5
   // jQuery AJAX - fetch might be a better fit (and is native), but AJAX is easy.
@@ -20,40 +21,61 @@ function Update() {
         // For each entry, add an entry showing the creator, timestamp, fork length, and lost rewards
         // Clickthrough should yield more complex information, this is just a brief overview and should be compact
         // HTML tables are ugly by default so I'm using a div, but this can be re-organized easily if this is a stupid idea
-        let forktable = document.createElement("div");
-        forktable.id = "Fork-Table";
+        let forkTable = document.createElement("div");
+        forkTable.id = "Fork-Table";
         // Add each entry to its own div which will be contained within Fork-Table
-        $.each(data, function(i, forks) {
-          // Div
-          let forkwrapper = document.createElement("div");
-          forkwrapper.className = "Fork-Wrapper";
-          let forkentry = document.createElement("div");
-          forkentry.className = "Fork-Entry";
-          forkentry.id = forks.id;
-          let forkdetails = document.createElement("div");
-          forkdetails.className = "Fork-Details";
-          forkdetails.id = forks.id;
-          // Data
-          let timestamp = new Date(forks.last_updated)
-          let localtimestamp = timestamp.toLocaleDateString('us-EN') + " at " + timestamp.toLocaleTimeString('us-EN');
-          forkentry.innerText = forks.latest + ", created on: " + localtimestamp;
-          forkwrapper.appendChild(forkentry);
+        for (var i = 0; i < max; i++) {
+          // Wrapper
+          /*-----------------------------------------------*\
+          | [TIMESTAMP]                                     |
+          |-------------------------------------------------|
+          | [LATEST FORK], BY [LATEST PRODUCER]             |
+          |-------------------------------------------------|
+          | [LENGTH] BLOCKS LONG, WITH [REWARDS] MINA LOST  |
+          \*-----------------------------------------------*/
+
+          let forkWrapper = document.createElement("div");
+          forkWrapper.className = "Fork-Wrapper";
+          forkWrapper.id = data[i].id;
+
+          // Timestamp
+          let forkTimestamp = document.createElement("div");
+          forkTimestamp.className = "Fork-Timestamp";
+          let timestamp = new Date(data[i].last_updated)
+          let localTimestamp = timestamp.toLocaleDateString('us-EN') + " at " + timestamp.toLocaleTimeString('us-EN');
+          forkTimestamp.innerText = localTimestamp;
+          forkWrapper.appendChild(forkTimestamp);
+
+          // Links
+          let forkLinks = document.createElement("div");
+          forkLinks.className = "Fork-Links";
+          let forkLatestLink = document.createElement("a");
+          let forkProducerLink = document.createElement("a");
+          forkLatestLink.href = "/Fork?fork=" + data[i].id;
+          forkLatestLink.innerText = data[i].latest;
+          forkProducerLink.innerText = ", by Placeholder Producer";
+          forkLinks.appendChild(forkLatestLink);
+          forkLinks.appendChild(forkProducerLink);
+          forkWrapper.appendChild(forkLinks);
+
           // Details
-          forkdetails.innerText = "Created by: " + forks.creators[forks.creators.length - 1] + ", fork length: " + forks.length;
-          forkwrapper.appendChild(forkdetails);
+          let forkDetails = document.createElement("div");
+          forkDetails.className = "Fork-Details";
+          forkDetails.innerText = data[i].length + " blocks long, with " + data[i].rewards + " MINA lost."
+          forkWrapper.appendChild(forkDetails);
+
           // Integration
-          forktable.appendChild(forkwrapper);
-        });
+          forkTable.appendChild(forkWrapper);
+        };
         // Replace loading text with content
-        $("#Fork-Table").replaceWith(forktable);
+        $("#Fork-Table").replaceWith(forkTable);
       });
     }
   });
-
   // Display a loading message while we wait for the API call
   return (
     <>
-      <div id="Fork-Header">Latest Forks:</div>
+      <div id="Fork-Header">Showing {max} forks from the past week:</div>
       <div id="Fork-Table">Loading...</div>
     </>
   );
@@ -63,8 +85,7 @@ function Update() {
 function GetParameter(parameter: any) {
   let url = window.location.search.substring(1);
   let urlVars = url.split('?');
-  var i;
-  for (i = 0; i < urlVars.length; i++) {
+  for (var i = 0; i < urlVars.length; i++) {
     // varName is an array, with index 0 being the key and 1 being the value
     let varName = urlVars[i].split('=');
     if (varName[0] === parameter) {
@@ -77,31 +98,76 @@ function GetParameter(parameter: any) {
 }
 
 // Display fork details
-// // TODO: API lookup using ID for detailed for information, add redirect if no fork query
+// // TODO: add redirect if no fork query
 function Details(parameter: any) {
   if (!parameter) {
     // Redirect should happen here if there is no fork at all
+    return <Redirect to="/Index" />
   }
-  return "Under Construction";
+  // AJAX call to the API for a specific fork
+  let API = 'https://fork-checker-api-iyrf2hryca-uw.a.run.app/forks?fork_id='
+  $.ajax({
+    url: API + parameter,
+    async: true,
+    dataType: 'json',
+    success: function(data) {
+      $(function() {
+        if (data.length === 0 || data[0] == undefined) {
+          // Malformed id will still successfully return an empty JSON, so redirect as though no parameter was given
+          window.location.href = '/Index';
+        }
+        let forkWrapper = document.createElement("div");
+        forkWrapper.className = "Fork-Wrapper";
+        forkWrapper.id = data[0].id;
+        let forkDetails = document.createElement("div");
+        forkDetails.className = "Fork-Details";
+        forkDetails.innerText = "Viewing Fork with latest block = " + data[0].latest + "\n Under Construction";
+        forkWrapper.appendChild(forkDetails);
+        $('#Fork-Details').replaceWith(forkWrapper);
+      });
+    }
+  });
+  // Loading...
+  return (
+    <>
+      <div id="Fork-Details">Loading...</div>
+    </>
+  );
 }
 
 // Main page
-// TODO: Implement react-router properly for this page
 const Index = () => (
   <div className="App">
+    <Router>
+      <Route path="/fork" component={ Fork } />
+    </Router>
     <header className="App-header">
-      {Update()}
+      {Update(5)}
     </header>
   </div>
 );
 
 // Fork page
 const Fork = () => {
-  var forkID = GetParameter('fork');
+  let forkID = GetParameter('fork');
+  return (
+    <div className="App">
+      <Router>
+        <Route path="/index" component={ Index } />
+      </Router>
+      <header className="App-header">
+        {Details(forkID)}
+      </header>
+    </div>
+  )
+};
+
+// Producer page
+// Huge TODO
+const Producer = () => {
   return (
     <div className="App">
       <header className="App-header">
-        {Details(forkID)}
       </header>
     </div>
   )
